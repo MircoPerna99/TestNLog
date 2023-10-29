16173558923856879adfa380a78027074a6de429
MircoPerna99 <mircoperna99@gmail.com>
2023-10-29 19:37:41 +0100

    Da json a oggetto

---
+++ TestNlog/Model/LogBase.cs
@@ -0,0 +1,19 @@
+﻿#nullable disable
+using System;
+namespace TestNlog.Model
+{
+	public class LogBase
+	{
+		public string Level { get; set; }
+	
+		public string Message { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public LogBase()
+		{
+		
+		}
+	}
+}
+
